20ae29861592dfe00d13fd394b83773287431434
matt-vdv <matthew.vandevyver@outlook.com>
2023-06-01 12:40:44 +0100

    Remove failing playwright test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -5,7 +5,6 @@ test('has title', async ({ page }) => {
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/Playwright/);
-    await expect(page).toHaveTitle('Test');
 });
 
 test('get started link', async ({ page }) => {
