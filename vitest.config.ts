ae13879fa9df39be7273848d6ad006cb8ef75b61
matt-vdv <matthew.vandevyver@outlook.com>
2023-06-01 11:50:56 +0100

    Run prettier

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  test: {
-    coverage: {
-      provider: 'istanbul',
-      reporter: ['text', 'html', 'cobertura']
-    }
-  }
-})
+    test: {
+        coverage: {
+            provider: 'istanbul',
+            reporter: ['text', 'html', 'cobertura'],
+        },
+    },
+});
