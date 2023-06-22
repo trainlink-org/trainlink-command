b0c0e114a9b4c30ca7999d83fca499352c3ccab5
matt-vdv <matthew.vandevyver@outlook.com>
2023-06-22 17:00:32 +0100

    Chore: Remove plugin

--- plugins/loadData.ts
+++
@@ -1,7 +0,0 @@
-import { useLocoStore } from '~/stores/locos';
-import { socket } from '~/utils/socketHelper';
-import { Loco } from '@trainlink-org/shared-lib';
-
-export default defineNuxtPlugin((nuxtApp) => {
-    console.log('Inside plugin');
-});
