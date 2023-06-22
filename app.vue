d7d6dac939b4640d2416820313ac2eb02bcd8f12
matt-vdv <matthew.vandevyver@outlook.com>
2023-06-22 16:58:41 +0100

    Feat: Move socket code to composable

--- app.vue
+++ app.vue
@@ -1,56 +1,6 @@
 <script setup lang="ts">
-// import { RouterView } from 'vue-router';
-
-// import NavComponent  from './components/NavComponent.vue';
-
 import { connected } from './utils/main';
-import { useLocoStore } from './stores/locos';
-import { Loco } from '@trainlink-org/shared-lib';
-const context = useNuxtApp();
-const locoStore = useLocoStore(context.$pinia);
-socket.on('metadata/initialState/locos', async (locosState) => {
-    console.log('New initial state');
-    for (const locoString of locosState) {
-        const loco = Loco.fromJson(JSON.parse(locoString));
-        locoStore.addLoco({
-            name: loco.name,
-            address: loco.address,
-            speed: loco.speed,
-            direction: loco.direction,
-            locked: false,
-            automationPID: '',
-        });
-        console.log('Added to store');
-    }
-});
-socket.on('automation/fetchRunningResponse', (automations) => {
-    console.log('Updating locked')
-    const usedLocos = automations.map((automation) => automation.locoAddress)
-    console.log(usedLocos)
-    locoStore.allLocos.forEach((loco) => {
-        if (!usedLocos.includes(loco.address)) {
-            locoStore.unlock(loco.address)
-        }
-    })
-    automations.forEach((automation) => {
-        if (automation.locoAddress) {
-            locoStore.lock(automation.locoAddress, automation.pid)
-        }
-    })
-})
-
-socket.on('throttle/speedUpdate', (identifier, speed, socketId) => {
-    if (typeof identifier === 'number' && socketId !== socket.id) {
-        const loco = locoStore.definedLocos.get(identifier);
-        if (loco) loco.speed = speed;
-    }
-});
-socket.on('throttle/directionUpdate', (identifier, direction) => {
-    if (typeof identifier === 'number') {
-        const loco = locoStore.definedLocos.get(identifier);
-        if (loco) loco.direction = direction;
-    }
-});
+useSocket()
 </script>
 
 <template>
--- stores/locos.ts
+++ stores/locos.ts
@@ -1,6 +1,5 @@
 // import { Loco } from '@trainlink-org/shared-lib';
 import { Direction } from '@trainlink-org/shared-lib';
-import { RefSymbol } from '@vue/reactivity';
 import { defineStore, acceptHMRUpdate } from 'pinia';
 
 export interface LocoClient {
@@ -16,8 +15,6 @@ export const useLocoStore = defineStore('locos', () => {
     const definedLocos = ref(new Map<number, LocoClient>());
     const nameLookup = ref(new Map<string, number>());
     const activeThrottles: Ref<LocoClient[]> = ref([]);
-    // const activeThrottles: Ref<ActiveThrottle> = ref({});
-    console.log('Inside Store');
 
     const allLocos = computed(() => {
         return Array.from(definedLocos.value.values());
@@ -56,20 +53,8 @@ export const useLocoStore = defineStore('locos', () => {
         });
         const loco = definedLocos.value.get(locoId);
         if (loco) {
-            // activeThrottles.value[throttleId] = new Proxy(loco, handler);
             activeThrottles.value[throttleId] = loco;
         }
-    }
-    function setSpeed(throttleId: number, speed: number) {
-        activeThrottles.value[throttleId].speed = speed;
-        console.log(activeThrottles.value[throttleId]);
-        const loco = activeThrottles.value[throttleId];
-        // socket.emit(
-        //     'throttle/setSpeed',
-        //     loco.address.value,
-        //     loco.speed.value,
-        //     throttleId
-        // );
     }
     function addressFromName(name: string) {
         return nameLookup.value.get(name);
@@ -98,7 +83,6 @@ export const useLocoStore = defineStore('locos', () => {
         locoNames,
         addLoco,
         removeLoco,
-        setSpeed,
         switchActiveThrottle,
         lock,
         unlock,
@@ -108,20 +92,3 @@ export const useLocoStore = defineStore('locos', () => {
 if (import.meta.hot) {
     import.meta.hot.accept(acceptHMRUpdate(useLocoStore, import.meta.hot));
 }
-// const store = useLocoStore();
-
-// store.definedLocos.set(0, {
-//     name: 'Test',
-//     address: 0,
-//     speed: 0,
-//     direction: Direction.stopped,
-// });
-
-// store.activeThrottles[0] = store.definedLocos.get(0) || {
-//     name: 'Test',
-//     address: 0,
-//     speed: 0,
-//     direction: Direction.stopped,
-// };
-
-// store.activeThrottles[0].speed = 2;
