f6627131369042bf020724a39def3c6ca530f9eb
matt-vdv <matthew.vandevyver@outlook.com>
2023-08-07 16:26:56 +0100

    Feat: finish rewriting calculateMidPoints

--- components/mapComponents/ConnectorComponent.vue
+++ components/mapComponents/ConnectorComponent.vue
@@ -13,7 +13,7 @@ const props = defineProps({
     activeRoute: { type: Boolean, required: true },
 });
 
-const midPoints = calculateMidPointsNew(props.start, props.points, props.end);
+const midPoints = calculateMidPoints(props.start, props.points, props.end);
 const path = computed(() => {
     let path = `M ${midPoints.startMid.x} ${midPoints.startMid.y}`;
 
@@ -31,151 +31,24 @@ function calculateMidPoints(
     points: Coordinate[],
     endPoint: Coordinate,
 ) {
-    let innerStart: Coordinate;
-    let innerEnd: Coordinate;
-    if (points.length > 0) {
-        innerStart = points[0];
-        innerEnd = points[points.length - 1];
-    } else {
-        innerStart = endPoint;
-        innerEnd = startPoint;
-    }
-    let newStartPoint: Coordinate;
-    let newEndPoint: Coordinate;
-    console.log(props.start);
+    const distanceFactor = 5;
     if (
-        Math.sqrt(
-            Math.pow(startPoint.x - endPoint.x, 2) +
-                Math.pow(startPoint.y - endPoint.y, 2),
-        ) <= 5
+        Math.hypot(endPoint.x - startPoint.x, endPoint.y - startPoint.y) <
+        distanceFactor
     ) {
-        console.log('Start and end too close!');
-        newStartPoint = {
-            x: (startPoint.x + endPoint.x) / 2,
-            y: (startPoint.y + endPoint.y) / 2,
+        const midPoint = {
+            x:
+                Math.abs(endPoint.x - startPoint.x) +
+                (startPoint.x < endPoint.x ? startPoint.x : endPoint.x),
+            y:
+                Math.abs(endPoint.y - startPoint.y) +
+                (startPoint.y < endPoint.x ? startPoint.x : endPoint.x),
         };
-        newEndPoint = {
-            x: (startPoint.x + endPoint.x) / 2,
-            y: (startPoint.y + endPoint.y) / 2,
+        return {
+            startMid: midPoint,
+            endMid: midPoint,
         };
-    } else {
-        {
-            const angle = Math.atan(
-                (innerStart.x - startPoint.x) / (innerStart.y - startPoint.y),
-            );
-            const yLength = Math.cos(angle) * 5;
-            const xLength = Math.sqrt(25 - Math.pow(yLength, 2));
-            console.log(
-                `Start: ${startPoint.y}, innerStart: ${
-                    innerStart.y
-                }\n\t${yLength} -> ${startPoint.y + yLength}`,
-            );
-            let xPoint = startPoint.x + xLength;
-            let yPoint = startPoint.y + yLength;
-            if (
-                (xPoint > startPoint.x && xPoint > innerStart.x) ||
-                (xPoint < startPoint.x && xPoint < innerStart.x)
-            ) {
-                xPoint = startPoint.x - xLength;
-                if (
-                    (xPoint > startPoint.x && xPoint > innerStart.x) ||
-                    (xPoint < startPoint.x && xPoint < innerStart.x)
-                ) {
-                    xPoint = Math.abs(endPoint.x - startPoint.x);
-                }
-            }
-            if (
-                (yPoint > startPoint.y && yPoint > innerStart.y) ||
-                (yPoint < startPoint.y && yPoint < innerStart.y)
-            ) {
-                console.log('Correction');
-                yPoint = startPoint.y - yLength;
-                if (
-                    (yPoint > startPoint.y && yPoint > innerStart.y) ||
-                    (yPoint < startPoint.y && yPoint < innerStart.y)
-                ) {
-                    yPoint = Math.abs(endPoint.y - startPoint.y);
-                    console.log('Secondary correction');
-                }
-            }
-            newStartPoint = {
-                x: xPoint,
-                y: yPoint,
-            };
-        }
-        {
-            const angle = Math.atan(
-                (innerEnd.x - endPoint.x) / (innerEnd.y - endPoint.y),
-            );
-            const yLength = Math.cos(angle) * 5;
-            const xLength = Math.sqrt(25 - Math.pow(yLength, 2));
-            let xPoint = endPoint.x + xLength;
-            let yPoint = endPoint.y + yLength;
-
-            if (
-                (xPoint > startPoint.x && xPoint > endPoint.x) ||
-                (xPoint < startPoint.x && xPoint < endPoint.x)
-            ) {
-                xPoint = endPoint.x - xLength;
-                if (
-                    (xPoint > startPoint.x && xPoint > endPoint.x) ||
-                    (xPoint < startPoint.x && xPoint < endPoint.x)
-                ) {
-                    xPoint = Math.abs(endPoint.x - startPoint.x);
-                }
-            }
-            if (
-                (yPoint > innerEnd.y && yPoint > endPoint.y) ||
-                (yPoint < innerEnd.y && yPoint < endPoint.y)
-            ) {
-                yPoint = endPoint.y - yLength;
-                if (
-                    (yPoint > innerEnd.y && yPoint > endPoint.y) ||
-                    (yPoint < innerEnd.y && yPoint < endPoint.y)
-                ) {
-                    yPoint = Math.abs(endPoint.y - startPoint.y);
-                }
-            }
-            newEndPoint = {
-                x: xPoint,
-                y: yPoint,
-            };
-        }
-        if (
-            Math.sqrt(
-                Math.pow(startPoint.x - innerStart.x, 2) +
-                    Math.pow(startPoint.y - innerStart.y, 2),
-            ) <= 5
-        ) {
-            newStartPoint = {
-                x: Math.abs(startPoint.x - innerStart.x),
-                y: Math.abs(startPoint.y - innerStart.y),
-            };
-        }
-        if (
-            Math.sqrt(
-                Math.pow(endPoint.x - innerEnd.x, 2) +
-                    Math.pow(endPoint.y - innerEnd.y, 2),
-            ) <= 5
-        ) {
-            newEndPoint = {
-                x: Math.abs(endPoint.x - innerEnd.x),
-                y: Math.abs(endPoint.y - innerEnd.y),
-            };
-        }
     }
-    console.log(`Start: (${newStartPoint.x}, ${newStartPoint.y})`);
-    console.log(`End: (${newEndPoint.x}, ${newEndPoint.y})`);
-    return {
-        startMid: newStartPoint,
-        endMid: newEndPoint,
-    };
-}
-function calculateMidPointsNew(
-    startPoint: Coordinate,
-    points: Coordinate[],
-    endPoint: Coordinate,
-) {
     let afterStart = endPoint;
     let beforeEnd = startPoint;
     if (points.length === 1) {
@@ -195,7 +68,6 @@ function calculateMidPointsNew(
         beforeEnd.y - endPoint.y,
     );
 
-    const distanceFactor = 5;
     let startMidPoint = {
         x:
             startPoint.x +
@@ -212,6 +84,8 @@ function calculateMidPointsNew(
             endPoint.y +
             (distanceFactor / endDistance) * (beforeEnd.y - endPoint.y),
     };
+    if (startDistance < distanceFactor) startMidPoint = afterStart;
+    if (endDistance < distanceFactor) endMidPoint = beforeEnd;
     return {
         startMid: startMidPoint,
         endMid: endMidPoint,
