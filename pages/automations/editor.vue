314c1d20141987c3906ccaf7492504309b39e651
matt-vdv <matthew.vandevyver@outlook.com>
2023-09-07 14:48:12 +0100

    Add basic editor implementation

---
+++ pages/automations/editor.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="h-full relative">
+        <Codemirror
+            v-model="code"
+            placeholder="Code goes here..."
+            :style="{ height: '100%' }"
+            :autofocus="true"
+            :indent-with-tab="true"
+            :tab-size="2"
+        />
+        <ButtonComponent
+            class="absolute bottom-0 right-0 m-2 bg-accent-400 border-accent-400 hover:bg-accent-300"
+            >Save</ButtonComponent
+        >
+    </div>
+</template>
+
+<script setup lang="ts">
+import { EditorView } from 'codemirror';
+import { EditorState } from '@codemirror/state';
+import { Codemirror } from 'vue-codemirror';
+const code = ref(`console.log('Hello, world!')`);
+</script>
