6881c72916121ab1ffea8af908e9c69e1228af5a
matt-vdv <matthew.vandevyver@outlook.com>
2023-12-03 17:42:09 +0000

    WIP: Fix modal formatting for overflows

--- components/ModalComponent.vue
+++ components/ModalComponent.vue
@@ -13,31 +13,31 @@ const emit = defineEmits<{
 
 <template>
     <div
-        class="fixed top-0 -left-2 z-20 h-screen w-screen bg-black opacity-25"
+        class="fixed top-0 left-0 z-20 h-screen w-screen bg-black opacity-25"
         @click="emit('cancel')"
     />
     <div
-        class="fixed top-0 -left-2 z-30 flex h-screen w-screen items-center justify-center bg-transparent"
+        class="fixed top-0 left-0 z-30 flex h-screen w-screen items-center justify-center bg-transparent"
         @click="emit('cancel')"
     >
         <div
-            class="flex h-1/3 w-2/3 flex-col justify-between rounded-lg border-4 border-borderColor-300 bg-white text-center md:w-1/3 fixed"
+            class="flex h-1/3 w-2/3 flex-col justify-between rounded-lg border-4 border-borderColor-300 bg-white text-center md:w-1/3 relative"
             @click="(event) => event.stopPropagation()"
         >
-            <div class="static">
-                <h1
-                    class="w-full border-b-2 border-borderColor-300 bg-transparent text-lg"
-                >
-                    {{ props.title }}
-                </h1>
+            <h1
+                class="w-full border-b-2 border-borderColor-300 bg-transparent text-lg"
+            >
+                {{ props.title }}
+            </h1>
+            <div class="static h-full overflow-scroll">
                 <!-- <span
                     class="border-b-2 border-borderColor-300 w-full"
                 /> -->
-                <div class="p-1 static">
+                <div class="p-1 static overflow-y-scroll">
                     <slot />
                 </div>
             </div>
-            <div class="flex justify-end space-x-1 p-1">
+            <div class="flex justify-end space-x-1 p-1 border-t-2 w-full">
                 <ButtonComponent
                     v-if="props.cancelText !== '_hidden_'"
                     class="border-2 border-primary-400 hover:bg-accent-100"
